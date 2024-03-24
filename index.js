a72ba3ea55d99812489b040348c6b09e591857e6
Preeti Singh <115339754+ghpreeti@users.noreply.github.com>
2024-03-24 21:13:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const express = require("express");
+const multer  = require('multer')
+
+const app = express();
+const PORT = 8000;
+
+// const upload = multer({ dest: 'uploads/' })
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+     
+      cb(null, `${Date.now()}-${file.filename}`)
+    },
+  });
+  
+  const upload = multer({ storage: storage })
+
+app.set("view engine","ejs");
+app.set("views",path.resolve("./views"));
+
+app.use(express.urlencoded({extended:false}));
+
+app.get('/',(req,res)=>{
+    return res.render("homepage")
+});
+
+app.post('/upload',upload.single("profileImage"),(req,res)=>{
+console.log(req.body);
+console.log(req.file);
+
+return res.redirect("/");
+})
+
+app.listen(PORT,()=>console.log(`Server  is running on port ${PORT}`));
